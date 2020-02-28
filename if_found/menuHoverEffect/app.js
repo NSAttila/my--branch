feeb06778a42074ddc04f3740f27f48ec9337e05
Nagy-Sebestyen Attila <41479616+NSAttila@users.noreply.github.com>
2020-02-28 16:54:20 +0100

    Add files via upload

---
+++ if_found/menuHoverEffect/app.js
@@ -0,0 +1,16 @@
+// function sayHello(name) {
+//     return "Hello " + name;
+// }
+// //Function call
+// console.log(sayHello("Javascript"));
+
+// sayHello = name => "Hello " + name;
+// // calling the function
+// console.log(sayHello("Javascript"));
+
+// hello = () => "Hello";
+// console.log(hello());
+
+// squareValue = n => n*n;
+
+// console.log(squareValue(3));
