ed8c110754670be3b5cba3e9212f6c8f56d0daf5
Nagy-Sebestyen Attila <41479616+NSAttila@users.noreply.github.com>
2020-03-17 17:01:10 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const eu_arr = new Array("Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Kosovo","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Monaco","Montenegro","The Netherlands","Norway","Poland","Portugal","Romania","San Marino","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey","Ukraine","United Kingdom","Vatican City",);
+const as_arr = new Array("Afganistan","Armenia","Azerbaijam","Bahrain","Bangladesh","Bhutan","Brunei","Cambodia","China","Cyprus","Georgia","India","Indonesia","Iran","Iraq","Isreal","Japan","Jordan","Kazakhstan","Kuwait","Kyrgyzstan","Laos","Lebanon","Malaysia","Maldives","Mongolia","Myanmar","Nepal","Oman","Pakistan","Qatar","Saudi Arabia","Singapore","South Korea","Sri Lanka","State of Palestine","Syria","Tajikistan","Thailand","Timor-Leste","Turkey","Turkmenistan","United Arab Emirates","Uzbekistan","Vietnam","Yemen")
+const na_arr = new Array("Bermuda","Canada","Greenland","Saint Pierre & Miquelon","United States")
+const sa_arr = new Array("Argentina","Bolivia","Brazil","Chile","Colombia","Ecuador","Falkland Islands","French Guiana","Guyana","Paraguay","Peru","Suriname","Uruguay","Venezuela")
+const ca_arr = new Array("Anguila","Antigua and Barbuda","Aruba",
+"Bahamas",
+"Barbados",
+"Belize","British Virgin Island",
+"Costa Rica",
+"Cuba",
+"Dominica",
+"Dominican Rep.",
+"El Salvador",
+"Grenada",
+"Guatemala",
+"Haiti",
+"Honduras",
+"Jamaica",
+"Mexico",
+"Nicaragua",
+"Panama",
+"St. Kitts & Nevis",
+"St. Lucia",
+"St. Vincent &",
+"the Grendines",
+"Trinidad & Tobago",
+"United States")
+
+
+// ,"Russia"
