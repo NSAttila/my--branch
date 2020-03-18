a8295a7c86c10c468d57c3e18e1123332bd94a4a
Nagy-Sebestyen Attila <41479616+NSAttila@users.noreply.github.com>
2020-03-18 09:21:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-const eu_arr = new Array("Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Kosovo","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Monaco","Montenegro","The Netherlands","Norway","Poland","Portugal","Romania","San Marino","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey","Ukraine","United Kingdom","Vatican City",);
+const eu_arr = new Array("Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Kosovo","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Monaco","Montenegro","The Netherlands","Norway","Poland","Portugal","Romania","Russia","San Marino","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey","Ukraine","United Kingdom","Vatican City",);
 const as_arr = new Array("Afganistan","Armenia","Azerbaijam","Bahrain","Bangladesh","Bhutan","Brunei","Cambodia","China","Cyprus","Georgia","India","Indonesia","Iran","Iraq","Isreal","Japan","Jordan","Kazakhstan","Kuwait","Kyrgyzstan","Laos","Lebanon","Malaysia","Maldives","Mongolia","Myanmar","Nepal","Oman","Pakistan","Qatar","Saudi Arabia","Singapore","South Korea","Sri Lanka","State of Palestine","Syria","Tajikistan","Thailand","Timor-Leste","Turkey","Turkmenistan","United Arab Emirates","Uzbekistan","Vietnam","Yemen")
 const na_arr = new Array("Bermuda","Canada","Greenland","Saint Pierre & Miquelon","United States")
 const sa_arr = new Array("Argentina","Bolivia","Brazil","Chile","Colombia","Ecuador","Falkland Islands","French Guiana","Guyana","Paraguay","Peru","Suriname","Uruguay","Venezuela")
-const ca_arr = new Array("Anguila","Antigua and Barbuda","Aruba",
-"Bahamas",
-"Barbados",
-"Belize","British Virgin Island",
-"Costa Rica",
-"Cuba",
-"Dominica",
-"Dominican Rep.",
-"El Salvador",
-"Grenada",
-"Guatemala",
-"Haiti",
-"Honduras",
-"Jamaica",
-"Mexico",
-"Nicaragua",
-"Panama",
-"St. Kitts & Nevis",
-"St. Lucia",
-"St. Vincent &",
-"the Grendines",
-"Trinidad & Tobago",
-"United States")
-
-
-// ,"Russia"
+const ca_arr = new Array("Anguila","Antigua and Barbuda","Aruba","Bahamas","Barbados","Belize","British Virgin Island","Caribbean Netherlands","Cayman Islands","Costa Rica","Cuba","Curacao","Dominica","Dominican Republic","El Salvador","Grenada","Guadelope","Guatemala","Haiti","Honduras","Jamaica","Martinique","Mexico","Montserrat","Nicaragua","Panama","Puerto Rico","St. Kitts & Nevis","St. Lucia","St. Martin","St. Vincent & Grenadines","Trinidad & Tobago","Turks and Caicos","U.S. Virgin Islands")
+const me_arr = new Array("Bahrain","Cyprus","Egypt","Iran","Iraq","Israel","Jordan","Kuwait","Lebanon","Oman","Palestine","Qatar","Saudi Arabia","Syria","Turkey","United Arab Emirates","Yemen")
+const ao_arr = new Array("Australia","Fiji","Kiribati","Marshall Islands","Micronesia","Nauru","New Zealand","Palau","Papua New Guinea","Samoa","Solomon Islands","Tonga","Tuvalu","Vanuatu")
+const af_arr = new Array("Algeria","Angola","Benin","Botswana","Burkina Faso","Burundi","Cameroon","Cape Verde","Central African Rep.","Chad","Comoros","Congo","Congo (Dem. Rep.)","Djibouti","Egypt","Equatorial Guinea","Eritrea","Eswatini","Ethiopia","Gabon","Gambia","Ghana","Guinea","Guinea-Bissau","Ivory Coast","Kenya","Lesotho","Liberia","Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco","Mozambique","Namibia","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan","Sudan","Swaziland","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe")
